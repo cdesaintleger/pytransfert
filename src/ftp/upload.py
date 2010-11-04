8d4ae8abbd833e7873d5e74911b3d3b39c256ddd
christophe de saint leger <cdesaintleger@creavi.fr>
2010-11-04 09:28:20 +0100

    Ajout des notifications par mail

--- src/ftp/upload.py
+++ src/ftp/upload.py
@@ -1,10 +1,24 @@
 # -*- coding: utf8 -*-
+
+#multitâche
 from threading import Thread
+
+#ftp
 from ftplib import FTP, error_perm, all_errors
+
+#log
 from Debug import Debug
+
+#Fichier de config
 import ConfigParser
-from time import sleep
+
+#ddb
 from bdd import acces_bd
+
+#mails
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 
 class MyFtp(Thread):
@@ -60,6 +74,9 @@ class MyFtp(Thread):
             if(cret == 0):
                 #Changement d'état en base => 3 upload terminé si tout est ok
                 self.sql.execute("UPDATE "+str(self.conf.get("DDB","TBL_ETAT"))+" SET "+str(self.conf.get("DDB","CHAMP_ETAT"))+" = 3 WHERE "+str(self.conf.get("DDB","CHAMP_ID"))+" = "+str(self.file[0]))
+
+                #notification 
+                self.notify_by_mail('data_newfilenotify')
 
         finally:
             #signalement de la fin de l'upload donc du thread
@@ -130,6 +147,115 @@ class MyFtp(Thread):
             f.close()
             #cloture de la connection FTP
             ftp.quit()
+
+
+    #Notification par mail de l'arrivé des fichiers ou d'un probléme quelconque
+    def notify_by_mail(self,mail_type):
+
+
+        maildata    =   self.dispatch(mail_type)
+
+        # me == my email address
+        # you == recipient's email address
+        me = maildata.get("from","pytransfert@rapid-flyer.com")
+
+        #recupére les adresses selon le type de mail à envoyer ( data_newfilenotify , data_emergencynotify )
+        you =   maildata.get("destinataire","pytransfert@rapid-flyer.com")
+
+
+        # Create message container - the correct MIME type is multipart/alternative.
+        msg = MIMEMultipart('alternative')
+        msg['Subject'] = maildata.get("sujet","notification pytransfert")
+        msg['From'] = me
+        msg['To'] = you
+
+        # Record the MIME types of both parts - text/plain and text/html.
+        part1 = MIMEText(text, 'plain')
+        part2 = MIMEText(html, 'html')
+
+        # Attach parts into message container.
+        # According to RFC 2046, the last part of a multipart message, in this case
+        # the HTML message, is best and preferred.
+        msg.attach(part1)
+        msg.attach(part2)
+
+        # Send the message via local SMTP server.
+        s = smtplib.SMTP('localhost')
+        # sendmail function takes 3 arguments: sender's address, recipient's address
+        # and message to send - here it is sent as one string.
+        s.sendmail(me, you, msg.as_string())
+        s.quit()
+
+
+
+
+        #Recupére les infos pour l'expédition de mail à l'arrive d'un nouveau fichier
+        def _data_newfilenotify(self):
+
+            data['from']    =   str(self.conf.get("NOTIFY","NEWFILEFROM"))
+            data['destinataires']    =   str(self.conf.get("NOTIFY","NEWFILEDEST"))
+            data['sujet']           =   str(self.conf.get("NOTIFY","NEWFILESUBJECT"))
+
+            # Create the body of the message (a plain-text and an HTML version).
+            data['text'] = "Hi!\nHow are you?\nHere is the link you wanted:\nhttp://www.python.org"
+            data['html'] = """\
+            <html>
+              <head></head>
+              <body>
+                <p>Hi!<br>
+                   How are you?<br>
+                   Here is the <a href="http://www.python.org">link</a> you wanted.
+                </p>
+              </body>
+            </html>
+            """
+
+            return data
+
+        #Recupére les infos pour l'expédition de mail à l'arrive d'un probléme 
+        def _data_emergencynotify(self):
+
+            data['from']    =   str(self.conf.get("NOTIFY","EMERGENCYFROM"))
+            data['destinataires']    =   str(self.conf.get("NOTIFY","EMERGENCYDEST"))
+            data['sujet']           =   str(self.conf.get("NOTIFY","EMERGENCYSUBJECT"))
+
+            # Create the body of the message (a plain-text and an HTML version).
+            data['text'] = "Hi!\nHow are you?\nHere is the link you wanted:\nhttp://www.python.org"
+            data['html'] = """\
+            <html>
+              <head></head>
+              <body>
+                <p>Hi!<br>
+                   How are you?<br>
+                   Here is the <a href="http://www.python.org">link</a> you wanted.
+                </p>
+              </body>
+            </html>
+            """
+
+            return data
+
+
+
+
+
+    #######################################
+    #
+    #           DISPATCH
+    #
+    #######################################
+
+
+
+    #emule le switch case .. qui n'hexiste pas en python
+    def _dispatch (self, value):
+
+        method_name = '_' + str(value)
+        method = getattr(self, method_name)
+        return method()
+
+
+
 
     
 
